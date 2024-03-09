7a7b6a5c46119776285f8e65cfc6144f2952d7f6
diligentliu <3391569802@qq.com>
2024-03-09 14:09:54 +0800

    两个任务切换

--- source/kernel/include/core/task.h
+++ source/kernel/include/core/task.h
@@ -5,11 +5,13 @@
 #include "cpu/cpu.h"
 
 typedef struct _task_t {
+	uint32_t *stack;
 	tss_t tss;
 	int tss_selector;
 } task_t;
 
 int task_init(task_t *task, uint32_t entry, uint32_t esp);
+void simple_switch(uint32_t *from, uint32_t *to);
 void task_switch_from_to(task_t *from, task_t *to);
 
 #endif //OS_TASK_H
