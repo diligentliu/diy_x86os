e7bbd86b46cab117507bfb5b66e7eda2836dafa5
diligentliu <3391569802@qq.com>
2024-03-16 23:03:51 +0800

    完成 yield 系统调用, 初步完成添加 newlib 库, todo: 实现适配层接口

--- source/shell/main.c
+++ source/shell/main.c
@@ -1,10 +1,20 @@
 #include "lib_syscall.h"
+// #include <stdio.h>
 
 int main(int argc, char *argv[]) {
+
+	int count = 0;
+
+	for (int i = 0; i < argc; ++i) {
+		print_msg("argv = %s", (int) argv[i]);
+	}
+
+	fork();
+	yield();
+
 	while (1) {
-		for (int i = 0; i < argc; ++i) {
-			print_msg("argv = %s", argv[i]);
-		}
+		print_msg("shell pid = %d", getpid());
+		print_msg("shell count = %d", count++);
 		msleep(1000);
 	}
 	return 0;
